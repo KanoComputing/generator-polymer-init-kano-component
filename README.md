0eee5f454c36417b5e0bd2255c9c68bf872195e6
Paul Varache <perso@paulvarache.ninja>
2017-09-05 17:36:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-polymer-init-kano-component [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> A Polymer component with integration to Kano&#39;s test and build pipeline
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-polymer-init-kano-component using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-polymer-init-kano-component
+```
+
+Then generate your new project:
+
+```bash
+yo polymer-init-kano-component
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [Kano Computing Ltd.]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-polymer-init-kano-component.svg
+[npm-url]: https://npmjs.org/package/generator-polymer-init-kano-component
+[travis-image]: https://travis-ci.org/KanoComputing/generator-polymer-init-kano-component.svg?branch=master
+[travis-url]: https://travis-ci.org/KanoComputing/generator-polymer-init-kano-component
+[daviddm-image]: https://david-dm.org/KanoComputing/generator-polymer-init-kano-component.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/KanoComputing/generator-polymer-init-kano-component
---
+++ generators/app/index.js
@@ -0,0 +1,75 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the impeccable ' + chalk.red('Kano Component') + ' generator!'
+    ));
+
+    const prompts = [{
+      type: 'list',
+      name: 'version',
+      message: 'What type of element would you like to build?',
+      choices: [{
+        name: 'Polymer 2.x',
+        value: 'polymer-2.x'
+      },{
+        name: 'Polymer hybrid',
+        value: 'polymer-hybrid'
+      }],
+      default: 'polymer-2.x'
+    }, {
+      type: 'input',
+      name: 'name',
+      message: 'What name should I give to your component?',
+      default: 'kano-component'
+    }, {
+      type: 'input',
+      name: 'description',
+      message: 'What is the purpose of this element? (description)'
+    }];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+
+  writing() {
+    const elementName = this.props.name;
+    const version = this.props.version;
+
+    // Generate the class name from the kebab cased attribute style name
+    const elementClassName = elementName.replace(/-([a-z])/g, (m, w) => {
+      return w.toUpperCase();
+    });
+
+    this.props.elementClassName = elementClassName.charAt(0).toUpperCase() + elementClassName.slice(1);
+
+    this.fs.copyTpl(
+      `${this.templatePath()}/${version}/**/!(_)*`,
+      this.destinationPath(),
+      this.props
+    );
+
+    this.fs.copyTpl(
+      `${this.templatePath()}/${version}/_element.html`,
+      this.destinationPath(`${elementName}.html`),
+      this.props
+    );
+
+    this.fs.copyTpl(
+      `${this.templatePath()}/${version}/test/_element_test.html`,
+      this.destinationPath(`test/${elementName}_test.html`),
+      this.props
+    );
+  }
+
+  install() {
+    this.bowerInstall();
+  }
+};
